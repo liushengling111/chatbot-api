037e86bfdce2ca96e665ccdfad9c469deb9e5c5a
liushengling <3184625880@qq.com>
2026-03-06 13:53:08 +0800

    260306-chatbot-api:init

---
+++ chatbot-api-interfaces/src/main/java/com/lsl/chatbot/interfaces/ApiApplication.java
@@ -0,0 +1,18 @@
+package com.lsl.chatbot.interfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liu'sheng'ling
+ * @description
+ * @create 2026/3/6 12:44
+ */
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
---
+++ chatbot-api-interfaces/src/test/java/com.lsl.chatbot.interfacces.test/ApiTest.java
@@ -0,0 +1,10 @@
+package com.lsl.chatbot.interfacces.test;
+
+/**
+ * @author liu'sheng'ling
+ * @description 单元测试
+ * @create 2026/3/6 12:47
+ */
+public class ApiTest {
+
+}
